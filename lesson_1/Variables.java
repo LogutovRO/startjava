27bf4ed5baef2bf95c36edccf8bf4bd89fcb3ea9
LogutovRO <joker2301@yandex.ru>
2021-08-15 11:12:50 +0300

    create folder lesson_1

---
+++ lesson_1/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte numCores = 6;
+        short ram = 12;
+        int bitRate = 500;
+        long serialNum = 49026347234l;
+
+        float freqCPU = 3.2f;
+        double buildNum = 120.221235300d;
+
+        char discName = 'D';
+
+        boolean current = true;
+
+        System.out.println(numCores);
+        System.out.println(ram);
+        System.out.println(bitRate);
+        System.out.println(serialNum);
+        System.out.println(freqCPU);
+        System.out.println(buildNum);
+        System.out.println(discName);
+        System.out.println(current);
+    }
+}
