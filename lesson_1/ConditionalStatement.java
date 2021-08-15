009d9fb3c2348d306a850e65b8cedca091c1c68d
LogutovRO <joker2301@yandex.ru>
2021-08-15 16:10:11 +0300

    move .gitignore

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,15 +1,11 @@
 public class ConditionalStatement {   
     public static void main(String[] args) {
-
         int age = 31;
-        boolean isSexMale = true;
-        float height = 1.88f;
-        char firstLetterName = 'R';
-
         if (age > 20) {
             System.out.println("Возраст больше 20 лет");
         }
 
+        boolean isSexMale = true;
         if (isSexMale) {
             System.out.println("Пол мужской");
         }
@@ -18,12 +14,14 @@ public class ConditionalStatement {
             System.out.println("Пол не мужской");
         }
 
+        float height = 1.88f;
         if (height < 1.80) {
             System.out.println("Рост ниже 1.8 метра");
         } else {
             System.out.println("Рост составляет " + height + " метра");
         }
 
+        char firstLetterName = 'R';
         if (firstLetterName == 'M') {
             System.out.println("Первая буква имени " + firstLetterName);
         } else if (firstLetterName == 'I') {
---
+++ lesson_1/Cycle.java
@@ -0,0 +1,29 @@
+public class Cycle {   
+    public static void main(String[] args) {
+        System.out.println("Task1: Выведите на консоль с помощью цикла for все числа от [0, 20]");
+
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("Task2: Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)");
+
+        int j = 6;
+        while (j >=-6) {
+            System.out.println("j = " + j);
+            j = j - 2;
+        }
+
+        System.out.println("Task3: Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20]. Выведите ее на консоль");
+
+        int k = 10;
+        int result = 0;
+        do {
+            if (k % 2 != 0) {
+                result = result + k;
+            }
+            k++;
+        } while (k <= 20);
+        System.out.println("result = " + result);
+    }
+}
